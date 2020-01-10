d64067d17ac75b9c512e4618db23cf3057409c9f
fuchsroey <fuchsroey@gmail.com>
2020-01-10 15:35:58 +0200

    all models are done, plot done

---
+++ Model_D.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Model_D(nn.Module):
+    def __init__(self, image_size):
+        super(Model_D, self).__init__()
+        self.image_size = image_size
+        self.fc1 = nn.Linear(image_size, 128)  # first layer
+        self.fc2 = nn.Linear(128, 64)  # second layer
+        self.fc3 = nn.Linear(64, 10)   # third layer
+        self.fc4 = nn.Linear(10, 10)   # forth layer
+        self.fc5 = nn.Linear(10, 10)   # fifth layer
+        self.fc6 = nn.Linear(10, 10)  # output
+
+
+    def forward(self, x):
+        x = x.view(-1, self.image_size)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = F.relu(self.fc5(x))
+        x = self.fc6(x)
+        return F.log_softmax(x, dim=1)
--- ex_4.py
+++ ex_4.py
@@ -3,10 +3,37 @@ from torchvision import transforms
 from torchvision import datasets
 from Model_A import Model_A
 from Model_B import Model_B
+from Model_C import Model_C
+from Model_D import Model_D
+from Model_E import Model_E
+
+
+
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 import torch.optim as optim
 import torch.nn.functional as F
+
+
+def plotDet(loss_train_set,acc_train_set,loss_valid_set,acc_valid_set ):
+    plt.figure()
+    plt.title('Avg. loss as function of epoch')
+    plt.ylabel('Avg. loss')
+    plt.xlabel('epoch #')
+    plt.plot(loss_train_set, label='training set')
+    plt.plot(loss_valid_set, label='validation set')
+    plt.legend()
+    plt.show()
+    plt.figure()
+    plt.title('Accuracy as function of epoch')
+    plt.ylabel('Accuracy')
+    plt.xlabel('epoch #')
+    plt.plot(acc_train_set, label='training set')
+    plt.plot(acc_valid_set, label='validation set')
+    plt.legend()
+    plt.show()
 
 
 def train(epoch, model):
@@ -19,38 +46,55 @@ def train(epoch, model):
         optimizer.step()
 
 
-def test(model):
+def test(model, check_loader):
     model.eval()
     test_loss = 0
     correct = 0
-    for data, target in test_loader:
+    for data, target in check_loader:
         output = model(data)
         test_loss += F.nll_loss(output, target, reduction='sum').item()
         pred = output.max(1, keepdim=True)[1]
         correct += pred.eq(target.view_as(pred)).cpu().sum()
 
-    test_loss /= len(test_loader.dataset)
+    test_loss /= len(check_loader.dataset)
+    acc = (100. * correct / len(check_loader.dataset))
     print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+        test_loss, correct, len(check_loader.dataset),
+        acc))
 
+    return test_loss, acc
 
 transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize((0,), (1,))])
 batchSize = 64
-train_loader = torch.utils.data.DataLoader(
-    datasets.FashionMNIST('./data', train=True, download=True, transform=transform),
-    batch_size=batchSize, shuffle=True
-)
+train_data_before_split = datasets.FashionMNIST('./data', train=True, download=True, transform=transform)
+
+train_size = int(len(train_data_before_split.train_data) * 0.8)
+valid_size = len(train_data_before_split.train_data) - train_size
+
+
+data_for_train, data_for_valid = torch.utils.data.random_split(train_data_before_split, [train_size, valid_size])
+
+train_loader = torch.utils.data.DataLoader(data_for_train, batch_size=batchSize, shuffle=True)
+validation_set = torch.utils.data.DataLoader(data_for_valid, batch_size=batchSize, shuffle=True)
 test_loader = torch.utils.data.DataLoader(
     datasets.FashionMNIST('./data', train=False, download=True, transform=transform),
     batch_size=batchSize, shuffle=True
 )
 
-model = Model_B(image_size=28 * 28)
+model = Model_E(image_size=28 * 28)
 lr = 0.001
 optimizer = optim.Adam(model.parameters(), lr=lr)
 
-for epoch in range(10):
+epohcs_number = 10
+
+loss_train_set = np.zeros(epohcs_number)
+acc_train_set = np.zeros(epohcs_number)
+loss_valid_set = np.zeros(epohcs_number)
+acc_valid_set = np.zeros(epohcs_number)
+for epoch in range(epohcs_number):
     train(epoch, model)
-    test(model)
+    loss_train_set[epoch], acc_train_set[epoch] = test(model, train_loader)
+    loss_valid_set[epoch], acc_valid_set[epoch] = test(model, validation_set)
+
+plotDet(loss_train_set, acc_train_set, loss_valid_set, acc_valid_set)
